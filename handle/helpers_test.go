33af16ed7d31794d02e77cb1a7b77552636849e2
fernandez14 <fernandez14@outlook.com>
2015-07-31 23:03:07 -0400

    [mod] Use convey for unit tests

--- handle/helpers_test.go
+++ handle/helpers_test.go
@@ -1,17 +1,26 @@
 package handle 
 
-import "testing"
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
 
 func TestSlugAscii(t *testing.T) {
+	
 	var tests = []struct{ in, out string }{
 		{"PC '¿Que opinan spartanos? 2.0'", "pc-que-opinan-spartanos-2-0"},
 		{"Clan de destiny ayudamos a todos :)", "clan-de-destiny-ayudamos-a-todos"},
 		{"AMD FX-6300 ó AMD Athlon x4 750k?", "amd-fx-6300-o-amd-athlon-x4-750k"},
 	}
-
-	for _, test := range tests {
-		if out := str_slug(test.in); out != test.out {
-			t.Errorf("%q: %q != %q", test.in, out, test.out)
+	
+	Convey("Make sure the slug generator works properly with weird characters", t, func() {
+		
+		for _, test := range tests {
+			
+			Convey(test.in + " should be " + test.out, func() {
+				
+				So(str_slug(test.in), ShouldEqual, test.out)
+			})
 		}
-	}
+	})
 }
