a09fd1974f21d413aac714018ddfa09b7499ff4b
Eduardo Fernandez <fernandez14@outlook.com>
2015-08-25 20:44:47 -0500

    [add] Category on feed

--- model/post.go
+++ model/post.go
@@ -93,6 +93,7 @@ type FeedPost struct {
 	Slug       string          `bson:"slug" json:"slug"`
 	Type       string          `bson:"type" json:"type"`
 	Categories []string        `bson:"categories" json:"categories"`
+	Category   bson.ObjectId   `bson:"category" json:"category"`
 	Comments   FeedComments    `bson:"comments" json:"comments"`
 	Author     User            `bson:"author,omitempty" json:"author,omitempty"`
 	UserId     bson.ObjectId   `bson:"user_id,omitempty" json:"user_id,omitempty"`
