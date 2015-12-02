4368e31eeb9efd6ac75e937ab7e55710e9ee322d
Eduardo Fernández <fernandez14@outlook.com>
2015-12-02 10:43:46 -0600

    [add] Generic gateway structure

---
+++ modules/gcommerce/gateway.go
@@ -0,0 +1,29 @@
+package gcommerce
+
+import (
+	"errors"
+)
+
+type Gateway interface {
+	
+	SettlePay()
+
+	// Set the Dependency injection module
+	SetDI(*Module)
+}
+
+func getGateway(name string) (Gateway, error) {
+
+	switch name {
+
+	case "offline":
+
+		return Gateway(GatewayOffline{}), nil
+	case "stripe":
+
+		return Gateway(GatewayStripe{}), nil
+	default:
+
+		return Gateway(GatewayDummy{}), errors.New("Invalid gateway id.")
+	}
+}
