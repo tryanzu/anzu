03f9374a0ecd7c38ada52d820603221c69ec5fcd
Eduardo Fernandez <fernandez14@outlook.com>
2015-12-03 16:35:46 -0600

    [add] Mandrill backward support

--- modules/api/controller/mail.go
+++ modules/api/controller/mail.go
@@ -22,6 +22,16 @@ type MailAPI struct {
 // Get an inbound mail from mandrill
 func (self MailAPI) Inbound(c *gin.Context) {
 
+	address := c.Param("address")
+
+	if address == "pc" {
+
+		// Fallback to mandrill handler
+		self.MandrillFallback(c)
+
+		return
+	}
+
 	request := c.Request
 	content, err := ioutil.ReadAll(request.Body)
 
@@ -94,6 +104,51 @@ func (self MailAPI) Inbound(c *gin.Context) {
 	c.JSON(200, gin.H{"status": "okay"})
 }
 
+func (self MailAPI) MandrillFallback(c *gin.Context) {
+
+	var events []MailMandrillInbound
+
+	mandrill_request := c.PostForm("mandrill_events")
+
+	err := json.Unmarshal([]byte(mandrill_request), &events)
+
+	if err != nil {
+
+		c.JSON(400, gin.H{"status": "Could not process the request the way we expected."})
+		return
+	}
+
+	// Due that we don't know how mandrill does stuff internally we'll respond back as fast as we can
+	go func(list []MailMandrillInbound, self MailAPI) {
+
+		database := self.Mongo.Database
+
+		for _, mail := range list {
+
+			// Assign an id to each mail so we can go further and relate them
+			id := bson.NewObjectId()
+			mail.Id = id
+
+			// Persist each inbound email 
+			err := database.C("mails").Insert(mail)
+
+			if err != nil {
+				continue
+			}
+
+			order, err := self.Store.OrderFinder(mail.Message.FromEmail)
+
+			if err == nil {
+
+				order.PushInboundAnswer(mail.Message.Text, id)
+			}
+		}
+
+	}(events, self)
+
+	c.JSON(200, gin.H{"status": "okay"})
+}
+
 type MailInbound struct {
 	Id        bson.ObjectId    `bson:"_id,omitempty" json:"id"`
 	MessageID string           `json:"MessageID"`
@@ -130,4 +185,30 @@ type MailAttachmentInbound struct {
 	ContentType string `json:"ContentType"`
 	ContentLength int `json:"ContentLength"`
 	ContentID string `json:"ContentID"`
+}
+
+type MailMandrillInbound struct {
+	Id        bson.ObjectId      `bson:"_id,omitempty" json:"id,omitempty"`
+	Timestamp int                `json:"ts"`
+	Event     string             `json:"event"`
+	Message   MailMandrillInboundMessage `json:"msg"`
+}
+
+type MailMandrillInboundMessage struct {
+	Headers   map[string]interface{} `json:"headers"`
+	Text      string                 `json:"text"`
+	FromEmail string                 `json:"from_email"`
+	FromName  string                 `json:"from_name"`
+	To        interface{}            `json:"to"`
+	Spam      interface{}            `json:"spam_report"`
+	Email     string                 `json:"email"`
+	Subject   string                 `json:"subject"`
+	Raw       interface{}            `json:"raw_msg"`
+	Dkim      interface{}            `json:"dkim"`
+	Spf       interface{}            `json:"spf"`
+	Sender    interface{}            `json:"sender"`
+	Tags      interface{}            `json:"tags"`
+	Html      string                 `json:"html"`
+	Flowed    interface{}            `json:"text_flowed"`
+	Template  interface{}            `json:"template"`
 }
