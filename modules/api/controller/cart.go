ad2dbf7a0d99c203341df259a15308ad510974b6
Eduardo Fernández <fernandez14@outlook.com>
2016-03-18 16:38:45 -0600

    [add] $remove_items_from_cart

--- modules/api/controller/cart.go
+++ modules/api/controller/cart.go
@@ -144,6 +144,7 @@ func (this CartAPI) Add(c *gin.Context) {
 func (this CartAPI) Delete(c *gin.Context) {
 
 	id := c.Param("id")
+	session_id := c.MustGet("session_id").(string)
 
 	if !bson.IsObjectIdHex(id) {
 		c.JSON(400, gin.H{"message": "Invalid request, check id format.", "status": "error"})
@@ -153,6 +154,8 @@ func (this CartAPI) Delete(c *gin.Context) {
 	container := this.getCart(c)
 	{
 		var items []CartComponentItem
+		var cartItem CartComponentItem
+		var matched bool = false
 
 		err := container.Bind(&items)
 
@@ -166,13 +169,46 @@ func (this CartAPI) Delete(c *gin.Context) {
 			if item.Id == id {
 
 				items[i].Quantity = items[i].Quantity - 1
+				cartItem = items[i]
 
 				if items[i].Quantity < 1 {
 					items = append(items[:i], items[i+1:]...)
 				}
 
+				matched = true
+
 				break
 			}
+		}
+
+		if matched {
+
+			user_id_i, signed_in := c.Get("user_id")
+
+			go func() {
+
+				cartItem.Quantity = 1
+				component_id := bson.ObjectIdHex(cartItem.Id)
+				component, err := this.Components.Get(component_id)
+
+				if err == nil {
+
+					data := map[string]interface{}{
+						"$session_id": session_id,
+						"$item": this.generateSiftItem(cartItem, component),
+					}
+
+					if signed_in {
+						data["$user_id"] = user_id_i.(string)
+					}
+
+					err := gosift.Track("$remove_item_from_cart", data)
+
+					if err != nil {
+						panic(err)
+					}
+				}
+			}()
 		}
 
 		container.Save(items)
