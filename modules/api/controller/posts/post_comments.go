47fc4c5f60de6b3cde50cb4c74278e7fc8a9a0e8
fernandez14 <fernandez14@outlook.com>
2016-04-14 17:47:09 -0500

    Post comments endpoint works with new API

--- modules/api/controller/posts/post_comments.go
+++ modules/api/controller/posts/post_comments.go
@@ -1,11 +1,9 @@
 package posts
 
 import (
-	"github.com/fernandez14/spartangeek-blacker/model"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/mgo.v2/bson"
 
-	"sort"
 	"strconv"
 )
 
@@ -14,11 +12,9 @@ func (this API) GetPostComments(c *gin.Context) {
 	post_id := c.Param("id")
 	offsetQuery := c.Query("offset")
 	limitQuery := c.Query("limit")
-	database := this.Mongo.Database
 
 	var offset int = 0
 	var limit int = 0
-	var post model.CommentsPost
 
 	if bson.IsObjectIdHex(post_id) == false {
 		c.JSON(400, gin.H{"message": "Invalid request, id not valid.", "status": "error"})
@@ -43,111 +39,31 @@ func (this API) GetPostComments(c *gin.Context) {
 	offset = offsetC
 	limit = limitC
 
-	// This will calculate the position based on the sliced array
-	count := 0
-	true_count := this.Feed.TrueCommentCount(id)
-
-	if offset >= 0 {
-		count = offset
-	} else {
-
-		offsetN := -offset
-
-		if offsetN > true_count {
-			offset = -true_count
-		}
-
-		count = true_count + offset
-	}
-
-	err = database.C("posts").FindId(id).Select(bson.M{"_id": 1, "comments.set": bson.M{"$slice": []int{offset, limit}}}).One(&post)
+	post, err := this.Feed.Post(id)
 
 	if err != nil {
-		c.JSON(404, gin.H{"message": "Couldnt found post with that id.", "status": "error"})
+		c.JSON(404, gin.H{"message": "Invalid request, post not found.", "status": "error"})
 		return
 	}
 
-	var users []model.User
-	var users_ls []bson.ObjectId
+	// Needed data loading to show post
+	post.LoadComments(limit, offset)
+	post.LoadUsers()
 
-	for _, c := range post.Comments.Set {
-		users_ls = append(users_ls, c.UserId)
-	}
-
-	err = database.C("users").Find(bson.M{"_id": bson.M{"$in": users_ls}}).All(&users)
-
-	if err != nil {
-		panic(err)
-	}
-
-	usersMap := make(map[bson.ObjectId]interface{})
-
-	var description string
-
-	for _, user := range users {
-		description = "Solo otro Spartan Geek más"
-
-		if len(user.Description) > 0 {
-			description = user.Description
-		}
-
-		usersMap[user.Id] = map[string]interface{}{
-			"id":          user.Id.Hex(),
-			"username":    user.UserName,
-			"description": description,
-			"image":       user.Image,
-			"level":       user.Gaming.Level,
-			"roles":       user.Roles,
-		}
-	}
-
-	// Name of the set to get
 	_, signed_in := c.Get("token")
-
-	// Look for votes that has been already given
-	var likes []model.Vote
 
 	if signed_in {
 
-		user_id := c.MustGet("user_id")
-		user_bson_id := bson.ObjectIdHex(user_id.(string))
+		user_str_id := c.MustGet("user_id")
+		user_id := bson.ObjectIdHex(user_str_id.(string))
 
-		// Get the likes given by the current user
-		_ = database.C("votes").Find(bson.M{"type": "comment", "related_id": post.Id, "user_id": user_bson_id}).All(&likes)
+		post.LoadVotes(user_id)
 	}
 
-	for index := range post.Comments.Set {
+	data := post.Data()
 
-		comment := &post.Comments.Set[index]
+	true_count := this.Feed.TrueCommentCount(data.Id)
+	data.Comments.Total = true_count
 
-		// Save the position over the comment
-		post.Comments.Set[index].Position = count + index
-
-		// Check if user liked that comment already
-		for _, vote := range likes {
-			if vote.NestedType == strconv.Itoa(index) {
-				post.Comments.Set[index].Liked = vote.Value
-			}
-		}
-
-		if _, okay := usersMap[comment.UserId]; okay {
-			post.Comments.Set[index].User = usersMap[comment.UserId]
-		}
-	}
-
-	// Remove deleted comments from the set
-	comments := post.Comments.Set[:0]
-
-	for _, c := range post.Comments.Set {
-		if c.Deleted.IsZero() == true {
-			comments = append(comments, c)
-		}
-	}
-
-	post.Comments.Set = comments
-
-	// Sort by created at
-	sort.Sort(model.ByCommentCreatedAt(post.Comments.Set))
-
-	c.JSON(200, post)
+	c.JSON(200, data)
 }
