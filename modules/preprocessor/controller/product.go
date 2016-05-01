285a82b25fe7e52bf592a02f2c28438258adb8c6
fernandez14 <fernandez14@outlook.com>
2016-04-30 19:21:59 -0500

    [fix] Compras-en-legion preprocessor

--- modules/preprocessor/controller/product.go
+++ modules/preprocessor/controller/product.go
@@ -4,21 +4,21 @@ import (
 	"github.com/fernandez14/spartangeek-blacker/modules/gcommerce"
 	"github.com/gin-gonic/gin"
 	"gopkg.in/mgo.v2/bson"
-	"strings"
-	"net/http"
 	"html"
+	"net/http"
+	"strings"
 )
 
 type ProductAPI struct {
 	GCommerce *gcommerce.Module `inject:""`
-	Page string
+	Page      string
 }
 
 func (this ProductAPI) Legion(c *gin.Context) {
-	
+
 	slug := c.Param("slug")
 
-    if slug == "" {
+	if slug == "" {
 
 		// Post not found, url hacked
 		c.Redirect(http.StatusMovedPermanently, "/")
@@ -28,9 +28,9 @@ func (this ProductAPI) Legion(c *gin.Context) {
 	if slug == "faq" {
 
 		this.FAQ(c)
-		return 
+		return
 	}
-	
+
 	products := this.GCommerce.Products()
 	product, err := products.GetByBson(bson.M{"slug": slug})
 
@@ -40,19 +40,19 @@ func (this ProductAPI) Legion(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/")
 		return
 	}
-    
-    var name string = html.EscapeString(product.Name)
-    var description string = html.EscapeString(name + " al mejor precio en México. Compra en Legión y aprovecha esta oferta por tiempo limitado.")
+
+	var name string = html.EscapeString(product.Name)
+	var description string = html.EscapeString(name + " al mejor precio en México. Compra en Legión y aprovecha esta oferta por tiempo limitado.")
 	var page string = this.Page
 
-	page = strings.Replace(page, "SpartanGeek.com | Comunidad de tecnología, geeks y más", "SpartanGeek.com | Compra en Legión | " + name , 1)
+	page = strings.Replace(page, "SpartanGeek.com | Comunidad de tecnología, geeks y más", "SpartanGeek.com | Compra en Legión | "+name, 1)
 	page = strings.Replace(page, "{{ page.title }}", name, 1)
 	page = strings.Replace(page, "{{ page.description }}", description, 2)
 
 	if len(product.Image) > 0 {
 
 		// First post image
-		page = strings.Replace(page, "{{ page.image }}", "https://assets.spartangeek.com/components/" + product.Image, 1)
+		page = strings.Replace(page, "{{ page.image }}", "https://assets.spartangeek.com/components/"+product.Image, 1)
 
 	} else {
 
@@ -64,8 +64,14 @@ func (this ProductAPI) Legion(c *gin.Context) {
 	c.String(200, page)
 }
 
+func (this *ProductAPI) ByPass(c *gin.Context) {
+
+	c.Header("Content-Type", "text/html; charset=utf-8")
+	c.String(200, this.Page)
+}
+
 func (this ProductAPI) FAQ(c *gin.Context) {
-	
+
 	var page string = this.Page
 
 	page = strings.Replace(page, "SpartanGeek.com | Comunidad de tecnología, geeks y más", "SpartanGeek.com | Compra en Legión | Preguntas frecuentes", 1)
