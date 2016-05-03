55be70bb89cf1bd90d20aaec6210584bc9b0f5d2
fernandez14 <fernandez14@outlook.com>
2016-05-03 18:19:17 -0500

    [deploy: production] fix starting price struct

--- modules/gcommerce/product.go
+++ modules/gcommerce/product.go
@@ -55,16 +55,15 @@ type MassdropFoundation struct {
 }
 
 type Massdrop struct {
-	Id            bson.ObjectId        `bson:"_id,omitempty" json:"id"`
-	ProductId     bson.ObjectId        `bson:"product_id" json:"product_id"`
-	Deadline      time.Time            `bson:"deadline" json:"deadline"`
-	Price         float64              `bson:"price" json:"price"`
-	StartingPrice float64              `bson:"price" json:"starting_price"`
-	Reserve       float64              `bson:"reserve_price" json:"reserve_price"`
-	Active        bool                 `bson:"active" json:"active"`
-	Shipping      time.Time            `bson:"shipping_date" json:"shipping_date"`
-	CoverSmall    string               `bson:"cover_small" json:"cover_small"`
-	Checkpoints   []MassdropCheckpoint `bson:"checkpoints" json:"checkpoints"`
+	Id          bson.ObjectId        `bson:"_id,omitempty" json:"id"`
+	ProductId   bson.ObjectId        `bson:"product_id" json:"product_id"`
+	Deadline    time.Time            `bson:"deadline" json:"deadline"`
+	Price       float64              `bson:"price" json:"price"`
+	Reserve     float64              `bson:"reserve_price" json:"reserve_price"`
+	Active      bool                 `bson:"active" json:"active"`
+	Shipping    time.Time            `bson:"shipping_date" json:"shipping_date"`
+	CoverSmall  string               `bson:"cover_small" json:"cover_small"`
+	Checkpoints []MassdropCheckpoint `bson:"checkpoints" json:"checkpoints"`
 
 	// Runtime generated data
 	Name         string      `bson:"-" json:"name,omitempty"`
@@ -78,7 +77,8 @@ type Massdrop struct {
 	Content string `bson:"content" json:"content"`
 
 	// Runtime generated data
-	Activities []MassdropActivity `bson:"-" json:"activities"`
+	Activities    []MassdropActivity `bson:"-" json:"activities"`
+	StartingPrice float64            `bson:"-" json:"starting_price"`
 
 	usersList interface{} `bson:"-" json:"-"`
 }
