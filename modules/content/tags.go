eccfb9ef16c397ec3994a94ef3af799f655e0477
fernandez14 <fernandez14@outlook.com>
2016-05-10 12:39:20 -0500

    [add] Asset tag parser

--- modules/content/tags.go
+++ modules/content/tags.go
@@ -1,10 +1,12 @@
 package content
 
 import (
-	"fmt"
 	"gopkg.in/mgo.v2/bson"
+
+	"fmt"
 	"regexp"
 	"strings"
+	"time"
 )
 
 var tag_regex, _ = regexp.Compile(`(?i)\[([a-z0-9]+(:?))+\]`)
@@ -46,6 +48,48 @@ func (self Module) ParseMentionTags(o Parseable, tags []Tag) bool {
 					if exists {
 
 						link := `<a class="user-mention" data-id="` + id + `" data-username="` + usr + `">@` + usr + `</a>`
+						c = strings.Replace(c, tag.Original, link, -1)
+					}
+				}
+			}
+		}
+
+		o.UpdateContent(c)
+	}
+
+	return true
+}
+
+func (self Module) ParseAssetTags(o Parseable, tags []Tag) bool {
+
+	if len(tags) > 0 {
+
+		var ids []string
+		c := o.GetContent()
+
+		for _, tag := range tags {
+
+			// Ensure tag for mentions and its params
+			if tag.Name == "asset" && len(tag.Params) > 0 {
+				if id := tag.Params[0]; bson.IsObjectIdHex(id) {
+					ids = append(ids, id)
+				}
+			}
+		}
+
+		assets := self.FetchAssetsHelper(ids)
+
+		for _, tag := range tags {
+
+			// Ensure tag for mentions and its params
+			if tag.Name == "asset" && len(tag.Params) > 0 {
+				if id := tag.Params[0]; bson.IsObjectIdHex(id) {
+
+					asset, exists := assets[id]
+
+					if exists {
+
+						link := asset
 						c = strings.Replace(c, tag.Original, link, -1)
 					}
 				}
@@ -107,8 +151,66 @@ func (self Module) FetchUsersHelper(ls []string) map[string]string {
 	return usrMap
 }
 
+func (self Module) FetchAssetsHelper(ls []string) map[string]string {
+
+	assetMap := map[string]string{}
+	redis := self.Redis
+	assets, err := redis.HMGet("assetsHash", ls...)
+
+	if err == nil && len(assets) > 0 {
+
+		for index, id := range ls {
+
+			// ls index should match users index
+			asset := assets[index]
+
+			if len(asset) > 0 {
+				assetMap[id] = string(asset)
+			}
+		}
+	}
+
+	missing := []bson.ObjectId{}
+
+	for _, id := range ls {
+		if _, exists := assetMap[id]; !exists {
+			missing = append(missing, bson.ObjectIdHex(id))
+		}
+	}
+
+	if len(missing) > 0 {
+
+		var targets []Asset
+
+		database := self.Mongo.Database
+		err := database.C("remote_assets").Find(bson.M{"_id": bson.M{"$in": missing}}).All(&targets)
+
+		if err == nil {
+			for _, asset := range targets {
+
+				var url string
+
+				if len(asset.Hosted) > 0 {
+					url = asset.Hosted
+				} else if len(asset.Original) > 0 {
+					url = asset.Original
+				}
+
+				assetMap[asset.Id.Hex()] = url
+
+				if asset.Status != "awaiting" {
+					go redis.HSet("assetsHash", asset.Id.Hex(), url)
+				}
+			}
+		}
+	}
+
+	return assetMap
+}
+
 func (self Module) ParseTags(o Parseable) error {
 
+	start := time.Now()
 	c := o.GetContent()
 	tags := []Tag{}
 	mtags := tag_regex.FindAllString(c, -1)
@@ -134,10 +236,9 @@ func (self Module) ParseTags(o Parseable) error {
 		}
 	}
 
-	fmt.Printf("%v\n", tags)
-
 	chain := []func(Parseable, []Tag) bool{
 		self.ParseMentionTags,
+		self.ParseAssetTags,
 	}
 
 	for _, fn := range chain {
@@ -149,6 +250,9 @@ func (self Module) ParseTags(o Parseable) error {
 	}
 
 	o.OnParseFinished()
+	elapsed := time.Since(start)
+
+	fmt.Printf("Parse tags took: %v nanoseconds\n", elapsed)
 
 	return nil
 }
