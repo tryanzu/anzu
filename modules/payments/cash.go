b2016378e104ae1c94107d06d5325cc30a544277
fernandez14 <fernandez14@outlook.com>
2016-05-31 17:43:54 -0500

    [add] Cash gateway

---
+++ modules/payments/cash.go
@@ -0,0 +1,36 @@
+package payments
+
+import (
+	"github.com/dustin/go-humanize"
+	"github.com/leebenson/paypal"
+)
+
+type Cash struct {
+	GenerateInvoice bool
+}
+
+func (p *Cash) GetName() string {
+	return "cash"
+}
+
+func (p *Cash) SetOptions(o map[string]interface{}) {
+
+	if i, exists := o["invoice"]; exists {
+		p.GenerateInvoice = i.(bool)
+	}
+}
+
+func (p *Cash) Purchase(pay *Payment, c *Create) (map[string]interface{}, error) {
+
+	response := map[string]interface{}{}
+	pay.Status = PAYMENT_SUCCESS
+
+	return response, nil
+}
+
+func (p *Cash) CompletePurchase(pay *Payment, data map[string]interface{}) (map[string]interface{}, error) {
+
+	response := map[string]interface{}{}
+
+	return response, nil
+}
