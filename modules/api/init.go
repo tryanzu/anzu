5b0464377d501109cd3291f6f584bb5f7963630d
fernandez14 <fernandez14@outlook.com>
2017-02-22 17:04:57 -0600

    Change login API Http Method.

--- modules/api/init.go
+++ modules/api/init.go
@@ -221,7 +221,7 @@ func (module *Module) Run() {
 		v1.GET("/users/:id/:kind", module.Users.UserGetActivity)
 		v1.GET("/user/search", module.Users.UserAutocompleteGet)
 		//v1.POST("/user/get-token/facebook", module.Users.UserGetTokenFacebook) DEPRECATED
-		v1.GET("/auth/get-token", module.Users.UserGetJwtToken)
+		v1.POST("/auth/get-token", module.Users.UserGetJwtToken)
 		v1.GET("/auth/lost-password", module.UsersFactory.RequestPasswordRecovery)
 		v1.GET("/auth/recovery-token/:token", module.UsersFactory.ValidatePasswordRecovery)
 		v1.PUT("/auth/recovery-token/:token", module.UsersFactory.UpdatePasswordFromToken)
